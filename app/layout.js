228a35d65fc3200227d3615b1975e294313a3fec
Ozan Cem YEŞİLBAŞ <130693372+ozancemyesilbas@users.noreply.github.com>
2026-07-16 15:37:21 +0300

    Load fragrance finder scroll styles

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css';
 import './finder.css';
+import './finder-scroll.css';
 import StoreProvider from '../components/StoreProvider';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
